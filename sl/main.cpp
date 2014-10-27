371b542e24eb05a8c9f9b8b3b79b2d521e32fa2a
Renato Marroquin <renatoj.marroquin@gmail.com>
2014-10-27 09:10:47 +0100

    Simple version of skipLists

---
+++ sl/main.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "skipLists.h"
+
+int main() {
+	
+	CSkiplist *sl = new CSkiplist();
+	sl->insert(8);
+	sl->iter();
+	sl->insert(7);
+	sl->iter();
+	sl->insert(6);
+	sl->iter();
+	sl->insert(10);
+	sl->iter();
+	if (sl->has(0))
+		printf("Si\n");
+	else
+		printf("No\n");
+	//printf("Hola Mu %i\n", 0);
+	return 0;
+}
